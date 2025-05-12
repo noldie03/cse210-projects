a61b7d679757b9aa9c215bb854cc58e8b2843f1a
Arnold Pring <pringalingling@gmail.com>
2025-05-12 07:22:39 -0600

    Working on Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Numerics;
+using System.Runtime.InteropServices.Marshalling;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of number, type 0 when finished.");
+        // List<int> numbers = new List<int>();
+        // Console.WriteLine("Enter a list of number, type 0 when finished.");
+
+        // int numInput = 1;
+        // int sum = 0;
+
+        // while (numInput != 0)
+        // {
+        //     Console.Write("Enter number: ");
+        //     string ansInput = Console.ReadLine();
+        //     numInput = int.Parse(ansInput);
+        //     numbers.Add(numInput);
+        // }
+      
+        // foreach (int num in numbers)
+        // {
+        //     sum += num;
+        // }
+
+        // Console.Write($"Total is: {sum}");
     }
 }
