f280397eb072c1d83613ea240f2933d6806ae0a7
Arnold Pring <pringalingling@gmail.com>
2025-05-12 01:03:32 -0600

    Exercise 3 in progress

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,34 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {   
+        int guess;
+        int num;
+        do
+        {
+            Console.Write("What is the magic number? ");
+            string ansNum = Console.ReadLine();
+            num = int.Parse(ansNum);
+
+            Console.Write("What is your guess? ");
+            string ansGuess = Console.ReadLine();
+            guess = int.Parse(ansGuess);
+
+            if (guess < num)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > num)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if(guess == num)
+            {
+                Console.WriteLine("Correct");
+            }
+        
+        } while (guess != num);
+
+
     }
 }
