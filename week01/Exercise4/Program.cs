e749aabb6234de1d679352c08f4ce2d6db3a503b
Arnold Pring <pringalingling@gmail.com>
2025-05-13 09:36:48 -0600

    Finished Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -14,20 +14,44 @@ class Program
 
         int numInput = 1;
         int sum = 0;
+        int max = 0;
+        int minPositive = 99999;
 
         while (numInput != 0)
         {
             Console.Write("Enter number: ");
             string ansInput = Console.ReadLine();
             numInput = int.Parse(ansInput);
-            numbers.Add(numInput);
+
+            if (numInput != 0)
+            {
+                numbers.Add(numInput);
+            }
         }
       
         foreach (int num in numbers)
         {
             sum += num;
-        }
 
-        Console.Write($"Total is: {sum}");
+            if (num > max)
+            {
+                max = num;
+            }
+            if (num > 0 && num < minPositive)
+            {
+                minPositive = num;
+            }
+        }
+        
+        float avg = ((float)sum) / numbers.Count;
+        
+        Console.WriteLine($"Total is: {sum}");
+        // Console.WriteLine($"Count is: {numbers.Count}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The largest number is : {max}");
+        Console.WriteLine($"The smallest positive number is {minPositive}");
+        Console.WriteLine("The sorted list is:");
+        numbers.Sort();
+        numbers.ForEach(Console.WriteLine);
     }
 }
