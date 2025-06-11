08d446043ae0830eb81aafc4b531d442826cc285
Arnold Pring <pringalingling@gmail.com>
2025-06-11 07:08:27 -0600

    Finished Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        string summary = assignment.GetSummary();
+        Console.WriteLine(summary);
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodrigues", "Fractions", "7.3", "8-19");
+        string mathSummary = mathAssignment.GetSummary();
+        string homework = mathAssignment.GetHomeworkList();
+        Console.WriteLine(mathSummary);
+        Console.WriteLine(homework);
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        string writingSummary = writingAssignment.GetSummary();
+        string writingInfo = writingAssignment.GetWritingInformation();
+        Console.WriteLine(writingSummary);
+        Console.WriteLine(writingInfo);
     }
 }
