a0254906e870c21d6d798657694a6b3a86c38509
Arnold Pring <pringalingling@gmail.com>
2025-06-14 11:47:57 -0600

    Finished Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 5);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Blue", 4, 6);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 3);
+        shapes.Add(s3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an Area of {area}.");
+        }
     }
 }
